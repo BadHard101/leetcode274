9170a083b485a2536a6c33b64ec5a271430bdf07
BadHard <BadHard101@yandex.ru>
2024-01-22 08:16:21 +0300

    Initial commit

---
+++ src/main/java/org/example/Solution1.java
@@ -0,0 +1,27 @@
+package org.example;
+
+class Solution1 {
+    public static int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+        for (int c : citations) {
+            if (c >= n) {
+                buckets[n]++;
+            } else {
+                buckets[c]++;
+            }
+        }
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hIndex(new int[]{3,0,6,1,5}));
+    }
+}
